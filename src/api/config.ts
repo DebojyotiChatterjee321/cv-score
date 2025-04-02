5567258b6022908aa40cfdfa598df9353984426c
DebojyotiChatterjee321 <debojyotichatterjee321@gmail.com>
2025-04-02 21:46:37 +0530

    Updating API endpiont

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,8 @@
 // Configuration for API endpoints
 // Contains base URL and endpoints for backend services
 
-export const API_BASE_URL = "http://localhost:8000";
+// export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://devdc-cv-jd-score-be.onrender.com";
 
 export const ENDPOINTS = {
   COMPUTE_CV_SCORE: `${API_BASE_URL}/compute_cvScore/`,
